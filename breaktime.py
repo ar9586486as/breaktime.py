a6106eec973b51d9c4400457e68d84a2c080966c
ar9586486as <37251108+ar9586486as@users.noreply.github.com>
2018-03-10 13:16:01 -0800

    Add files via upload

---
+++ breaktime.py
@@ -0,0 +1,9 @@
+import time
+import webbrowser
+total_breaks=5
+break_count=0
+
+print("this program started on"+time.ctime())
+time.sleep(10)
+webbrowser.open("https://www.youtube.com/watch?v=3ChgRbqGi-E")
+break_count=break_count+1
